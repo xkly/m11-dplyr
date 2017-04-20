bebcbb8564bb208adae129e720df991aec2c08b2
xkly <kly22@uw.edu>
2017-04-19 17:35:19 -0700

    worked on asking a question for exercise 5

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -22,23 +22,29 @@ print(str(pulitzer))
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2014 and Pultizer Prize Winners from 1990 to 2003.
-
-# pulitzer$Pulitzer.Prize.Change <- abs(pulitzer$Pulitzer.Prize.Winners.and.Finalists..2004.2014 - pulitzer$Pulitzer.Prize.Winners.and.Finalists..1990.2003)
-
-# Using dplyr
 Pulitzer.Prize.Change <- mutate(pulitzer, difference.2004.2014.and.1990.2003 = 
                                 Pulitzer.Prize.Winners.and.Finalists..2004.2014 -
                                 Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
-
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
-most.prizes <- pulitzer %>%
-  filter()
+most.prizes <- Pulitzer.Prize.Change %>%
+  filter(difference.2004.2014.and.1990.2003 == max(difference.2004.2014.and.1990.2003)) %>%
+  select(Newspaper)
 
 # Which publication with at least 5 Pulitzers won from 2004-2014 had the biggest decrease(negative) in Daily circulation numbers? 
 # This publication should have Pulitzer prizes won a minimum of 5 Pulitzers, as well as the biggest decrease in circulation
-
+over.5.decrease <- Pulitzer.Prize.Change %>%
+  filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 >= 5) %>%
+  # we're comparing char though?
+  filter(Change.in.Daily.Circulation..2004.2013 == min(Change.in.Daily.Circulation..2004.2013)) %>%
+  select(Newspaper)
 
 # Your turn! An important part about being a data scientist is asking questions. 
-# Create a question and use dplyr to figure out the answer.  
+# Create a question and use dplyr to figure out the answer.
+# Question: Which publication decreased in number of Pulitzer prizes won but increased
+# their daily circulation the most from 2004?
+increase.circulation.lost.prizes <- filter(Pulitzer.Prize.Change, difference.2004.2014.and.1990.2003 < 0) %>%
+  filter(Change.in.Daily.Circulation..2004.2013 == max(Change.in.Daily.Circulation..2004.2013)) %>%
+  select(Newspaper)
+
